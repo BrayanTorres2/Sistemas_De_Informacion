3c65d2bb65292eb833bef9667d291d84a55d4c7a
Brayan Torres <47356178+BrayanTorres2@users.noreply.github.com>
2021-09-27 17:29:15 -0500

    Update power_bi.sql

--- power_bi.sql
+++ power_bi.sql
@@ -11,3 +11,28 @@ case
   ,a.CountryRegionCode,
   [Group] as Grupo
 from [Sales].[SalesTerritory] as a;
+
+select
+P.ProductID,
+P.Name AS 'Producto',
+S.Name as 'Subcategoria',
+C.Name as 'Categoria' 
+from [Production].[Product] AS p
+left join [Production].[ProductCategory] as S
+on P.ProductSubcategoryID=S.ProductCategoryID
+left join [Production].[ProductCategory] as C
+on s.ProductCategoryID= C.ProductCategoryID
+
+select 
+H.SalesOrderID,
+CAST(H.OrderDate as date) as Fecha_pedido,
+CAST(H.ShipDate as date) as Fecha_envio,
+H.TerritoryID, H.CustomerID,
+D.ProductID,
+D.OrderQty as unidades,
+D.UnitPrice as precio_unitario,
+D.OrderQty*D.UnitPrice as SubTotal
+from 
+[Sales].[SalesOrderHeader] as H
+left join [Sales].[SalesOrderDetail] as D
+on H.SalesOrderID=D.SalesOrderID

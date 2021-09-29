785980067337875f3e4051ba6dd318cc1aebf529
Brayan Torres <47356178+BrayanTorres2@users.noreply.github.com>
2021-09-29 15:31:39 -0500

    Update power_bi.sql

--- power_bi.sql
+++ power_bi.sql
@@ -36,3 +36,42 @@ from
 [Sales].[SalesOrderHeader] as H
 left join [Sales].[SalesOrderDetail] as D
 on H.SalesOrderID=D.SalesOrderID
+
+
+
+create procedure Generar_fechas
+as
+	Truncate Table Calendario;
+	declare @fec_inicio date;
+	declare @fec_fin date;
+	declare @año int;
+	declare @mes_num int;
+	declare @mes_corto varchar(20);
+	declare @mes_largo varchar(20);
+	declare @dia int;
+
+set @fec_inicio=(select CAST(min (OrderDate) as date ) from [Sales].[SalesOrderHeader]);
+set @fec_fin=(select CAST(max (ShipDate) as date ) from [Sales].[SalesOrderHeader]);
+
+while @fec_inicio<= @fec_fin
+begin 
+	set @año=(select YEAR(@fec_inicio));
+	set @mes_num=(select MONTH(@fec_inicio));
+	set @mes_corto=(select FORMAT(@fec_inicio, 'MMM','PE'));
+	SET @mes_largo=(select FORMAT(@fec_inicio, 'MMMM','es-PE' ));
+	set @dia=(select day (@fec_inicio))
+	INSERT into  Calendario(fecha,año,mes_numero,mes,mes_largo,dia)
+	select @fec_inicio,@año,@mes_num,@mes_corto,@mes_largo,@dia;
+	set @fec_inicio=DATEADD(DAY,1,@fec_inicio)
+end;
+drop procedure Generar_fechas
+exec  Generar_fechas
+create table Calendario(
+fecha date,
+año int,
+mes_numero int,
+mes varchar (3),
+mes_largo varchar (20),
+dia int
+)
+select * from Calendario
